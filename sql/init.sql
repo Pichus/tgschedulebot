430cfbd0b1ae384146af46fe77b4cd12c763c3dd
pichus <dimatyzuk@gmail.com>
2025-02-01 15:28:01 +0200

    edited init.sql so that the database is normalized

--- sql/init.sql
+++ sql/init.sql
@@ -1,17 +1,28 @@
-CREATE TABLE IF NOT EXISTS Chats (
-    ChatID INTEGER NOT NULL PRIMARY KEY,
-    ChatTelegramID TEXT NOT NULL,
-    MessageThreadID TEXT NOT NULL,
-    ChatName TEXT NOT NULL,
-    HighSchedule TEXT,
-    LowSchedule TEXT,
-    ScheduleMessageID INTEGER,
-    UserID INTEGER NOT NULL,
-    FOREIGN KEY(UserID) REFERENCES Users(UserID)
-);
-
 CREATE TABLE IF NOT EXISTS Users (
-    UserID INTEGER NOT NULL PRIMARY KEY,
+    UserID INTEGER PRIMARY KEY,
     UserTelegramID TEXT NOT NULL,
     UserName TEXT NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS Chats (
+    ChatID INTEGER PRIMARY KEY ,
+    UserID INTEGER NOT NULL REFERENCES Users (UserID),
+    ChatTelegramID TEXT NOT NULL,
+    ChatName TEXT NOT NULL,
+    MessageThreadID TEXT,
+    ScheduleMessageToEditID INTEGER
+);
+
+CREATE TABLE IF NOT EXISTS ScheduleTypes (
+    ScheduleTypeID INTEGER PRIMARY KEY,
+    ScheduleType VARCHAR(100) NOT NULL
+);
+
+INSERT INTO ScheduleTypes (ScheduleType) VALUES ('low'), ('high');
+
+CREATE TABLE IF NOT EXISTS Schedules (
+    ScheduleID INTEGER PRIMARY KEY,
+    ChatID INTEGER NOT NULL REFERENCES Chats (ChatID),
+    ScheduleType VARCHAR(100) NOT NULL REFERENCES ScheduleTypes (ScheduleType),
+    Schedule TEXT NOT NULL
+)
