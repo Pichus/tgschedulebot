af72eb9234de2c0f4211bb9a4a6616ff508ffe37
pichus <dimatyzuk@gmail.com>
2025-02-02 12:09:12 +0200

    made primary keys serial so that they automatically increment in postgres

--- sql/init.sql
+++ sql/init.sql
@@ -1,11 +1,11 @@
 CREATE TABLE IF NOT EXISTS Users (
-    UserID INTEGER PRIMARY KEY,
+    UserID SERIAL PRIMARY KEY,
     UserTelegramID INTEGER NOT NULL,
     UserName TEXT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS Chats (
-    ChatID INTEGER PRIMARY KEY ,
+    ChatID SERIAL PRIMARY KEY,
     UserID INTEGER NOT NULL REFERENCES Users (UserID),
     ChatTelegramID INTEGER NOT NULL,
     ChatName TEXT NOT NULL,
@@ -14,14 +14,14 @@ CREATE TABLE IF NOT EXISTS Chats (
 );
 
 CREATE TABLE IF NOT EXISTS ScheduleTypes (
-    ScheduleTypeID INTEGER PRIMARY KEY,
-    ScheduleType VARCHAR(100) NOT NULL
+    ScheduleTypeID SERIAL PRIMARY KEY,
+    ScheduleType VARCHAR(100) NOT NULL UNIQUE
 );
 
-INSERT INTO ScheduleTypes (ScheduleType) VALUES ('low'), ('high');
+INSERT INTO ScheduleTypes (ScheduleType) VALUES ( 'low'), ('high');
 
 CREATE TABLE IF NOT EXISTS Schedules (
-    ScheduleID INTEGER PRIMARY KEY,
+    ScheduleID SERIAL PRIMARY KEY,
     ChatID INTEGER NOT NULL REFERENCES Chats (ChatID),
     ScheduleType VARCHAR(100) NOT NULL REFERENCES ScheduleTypes (ScheduleType),
     Schedule TEXT NOT NULL
