dd995f340b64dbd716a1f4df63eb9aa405b066ab
pichus <dimatyzuk@gmail.com>
2025-02-05 20:02:33 +0200

    changed repository_base.py so that it is not needed to use with cursor in child class functions anymore

--- repositories/repository_base.py
+++ repositories/repository_base.py
@@ -1,14 +1,18 @@
-from psycopg import AsyncConnection
+from psycopg import AsyncConnection, Cursor, AsyncCursor
 import config
 
 
 class RepositoryBase:
     def __init__(self):
-        self._db_connection: AsyncConnection
+        self._db_connection: AsyncConnection | None = None
+        self._cursor: AsyncCursor | None = None
     async def __aenter__(self):
-        self._db_connection: AsyncConnection = await AsyncConnection.connect(config.database_connection_string)
+        self._db_connection = await AsyncConnection.connect(config.database_connection_string)
+        self._cursor: AsyncCursor = self._db_connection.cursor()
         return self
 
     async def __aexit__(self, exc_type, exc_val, exc_tb):
+        if self._cursor:
+            await self._cursor.close()
         if self._db_connection:
             await self._db_connection.close()
