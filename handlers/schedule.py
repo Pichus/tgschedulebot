16f38e55a4b7df455e6d626cee67f47e9a743619
pichus <dimatyzuk@gmail.com>
2025-02-05 20:53:16 +0200

    fixed naming issues regarding repositories

--- handlers/schedule.py
+++ handlers/schedule.py
@@ -1,14 +1,14 @@
 import datetime
-
 from aiogram import Router, F
 from aiogram.filters import Command, StateFilter
 from aiogram.fsm.state import StatesGroup, State
-from aiogram.types import Message, ReplyKeyboardRemove, KeyboardButton, ReplyKeyboardMarkup
+from aiogram.types import Message, ReplyKeyboardRemove
 from aiogram.fsm.context import FSMContext
 import utils
 from keyboards.chat_choice import chat_choice_keyboard
 
 from keyboards.schedule_choice import schedule_type_choice_keyboard
+from repositories import UserRepository, ChatRepository
 
 router = Router()
 
@@ -21,13 +21,12 @@ class ChooseScheduleType(StatesGroup):
 
 @router.message(StateFilter(None), Command("add_schedule"))
 async def cmd_add_schedule(message: Message, state: FSMContext):
-    users_service = UsersDatabaseService()
-    with users_service as service:
-        user_chats = service.get_user_chats(message.from_user.id)
+    with UserRepository as user_repository:
+        user_chats = user_repository.get_user_chats(message.from_user.id)
 
     chat_names = []
     for chat in user_chats:
-        chat_names.append(chat["ChatName"])
+        chat_names.append(chat["chat_name"])
 
     await message.answer("В якому чаті ви бажаєте оновити розклад?",
                          reply_markup=chat_choice_keyboard(chat_names))
@@ -55,14 +54,13 @@ async def enter_schedule(message: Message, state: FSMContext):
 @router.message(ChooseScheduleType.sending_schedule, F.text)
 async def response(message: Message, state: FSMContext):
     user_data = await state.get_data()
-    chats_service = ChatsDatabaseService()
-    with chats_service as service:
+    with ChatRepository() as chat_repository:
         if user_data["chosen_schedule_type"] == "нижній":
             print(message.text)
-            service.update_low_schedule(message.text, user_data["chosen_chat"])
+            chat_repository.update_low_schedule(message.text, user_data["chosen_chat"])
         elif user_data["chosen_schedule_type"] == "верхній":
             print(message.text)
-            service.update_high_schedule(message.text, user_data["chosen_chat"])
+            chat_repository.update_high_schedule(message.text, user_data["chosen_chat"])
 
     await message.answer("Успішно додано розклад")
     await state.clear()
@@ -70,14 +68,12 @@ async def response(message: Message, state: FSMContext):
 
 @router.message(Command("send_schedule"))
 async def cmd_send_schedule(message: Message):
-    chats_service = ChatsDatabaseService()
-
-    with chats_service as service:
-        if not service.chat_exists(message.chat.id):
+    with ChatRepository() as chat_repository:
+        if not chat_repository.chat_exists(message.chat.id):
             await message.answer("Цього чату немає в базі даних бота. Додайте його командою /add_chat")
             return
 
-        schedules = service.get_schedules(message.chat.id)
+        schedules = chat_repository.get_schedules(message.chat.id)
         current_schedule: str
 
         if utils.is_high_week(datetime.datetime.now().isocalendar().week):
@@ -86,4 +82,4 @@ async def cmd_send_schedule(message: Message):
             current_schedule = schedules["low"]
 
         bot_message = await message.answer(current_schedule)
-        service.add_schedule_message_id(bot_message.message_id, message.chat.id)
+        chat_repository.add_schedule_message_id(bot_message.message_id, message.chat.id)
