4f24354318ea48c1065c413e33ec6d82663d29d5
pichus <dimatyzuk@gmail.com>
2025-02-05 21:02:25 +0200

    updated chat_repository.py style to match repository_base.py

--- repositories/chat_repository.py
+++ repositories/chat_repository.py
@@ -1,67 +1,59 @@
 from models import ChatModel
 from repositories.repository_base import RepositoryBase
+from psycopg import sql
 
 
 class ChatRepository(RepositoryBase):
-    async def chat_exists(self, chat_telegram_id) -> bool:
-        async with self._db_connection.cursor() as cursor:
-            await cursor.execute("SELECT EXISTS(SELECT 1 FROM chats WHERE chat_telegram_id = %s)", (chat_telegram_id,))
-            user = await cursor.fetchone()
+    async def chat_exists(self, chat_telegram_id: int) -> bool:
+        query = str("SELECT EXISTS(SELECT 1 FROM chats WHERE chat_telegram_id = %s)")
+        await self._cursor.execute(query, (chat_telegram_id,))
+        user = await self._cursor.fetchone()
 
         return user[0]
 
-    # noinspection PyTypeChecker,PyArgumentList,SqlInsertValues
-    async def add_chat(self, chat_telegram_id, message_thread_id, chat_name, user_telegram_id) -> bool:
-        if await self.chat_exists(chat_telegram_id):
+    async def add_chat(self, chat_model: ChatModel, user_telegram_id: int) -> bool:
+        if await self.chat_exists(chat_model.chat_telegram_id):
             return False
 
-        async with self._db_connection.cursor() as cursor:
-            await cursor.execute("SELECT UserID FROM Users WHERE UserTelegramID = %s", (user_telegram_id,))
-            user_id = await cursor.fetchone()["UserID"]
-            await cursor.execute(
-                "INSERT INTO Chats (ChatTelegramID, MessageThreadID, ChatName, UserID)"
-                "VALUES (%s, %s, %s, %s)",
-                (chat_telegram_id, message_thread_id, chat_name, int(user_id)))
+        query1 = sql.SQL("SELECT user_id FROM users WHERE user_telegram_id = %s")
+        await self._cursor.execute(query1, (user_telegram_id,))
+        user_id = await self._cursor.fetchone()["user_id"]
+
+        query2 = sql.SQL(
+            "INSERT INTO chats (chat_telegram_id, message_thread_id, chat_name, user_id) VALUES (%s, %s, %s, %s)")
+        await self._cursor.execute(query2,(chat_model.chat_telegram_id, chat_model.message_thread_id,
+                                           chat_model.chat_name, int(user_id)))
+
         await self._db_connection.commit()
 
         return True
 
     async def update_low_schedule(self, low_schedule: str, chat_name):
-        async with self._db_connection.cursor() as cursor:
-            await cursor.execute("UPDATE Chats SET LowSchedule = ? WHERE ChatName = ?",
-                                  (low_schedule, chat_name))
+        query = sql.SQL("UPDATE chats SET low_schedule = %s WHERE chat_name = %s")
+        await self._cursor.execute(query,(low_schedule, chat_name))
         await self._db_connection.commit()
 
     async def update_high_schedule(self, high_schedule, chat_name):
-        async with self._db_connection.cursor() as cursor:
-            await cursor.execute("UPDATE Chats SET HighSchedule = ? WHERE ChatName = ?",
-                                     (high_schedule, chat_name))
+        query = sql.SQL("UPDATE chats SET high_schedule = %s WHERE chat_name = %s")
+        await self._cursor.execute(query,(high_schedule, chat_name))
         await self._db_connection.commit()
 
     async def get_schedules(self, chat_telegram_id) -> dict[str, str]:
-        async with self._db_connection.cursor() as cursor:
-            await cursor.execute("SELECT HighSchedule, LowSchedule FROM Chats WHERE ChatTelegramID = ?",
-                              (chat_telegram_id,))
-            result = await cursor.fetchone()
+        query = sql.SQL("SELECT high_schedule, low_schedule FROM chats WHERE chat_telegram_id = %s")
+        await self._cursor.execute(query,(chat_telegram_id,))
+        result = await self._cursor.fetchone()
 
         return {"high": result[0], "low": result[1]}
 
-    # noinspection SqlResolve,PyTypeChecker
     async def add_schedule_message_id(self, schedule_message_id, chat_telegram_id):
-        async with self._db_connection.cursor() as cursor:
-            await cursor.execute("UPDATE Chats "
-                              "SET ScheduleMessageID = ?"
-                              "WHERE ChatTelegramID = ?",
-                              (schedule_message_id, chat_telegram_id))
+        query = sql.SQL("UPDATE chats SET schedule_message_id = %s WHERE chat_telegram_id = %s")
+        await self._cursor.execute(query,(schedule_message_id, chat_telegram_id))
         await self._db_connection.commit()
 
-    # noinspection PyTypeChecker
     async def get_chats_for_edit(self) -> list[ChatModel]:
-        async with self._db_connection.cursor() as cursor:
-            await cursor.execute("SELECT * "
-                              "FROM Chats "
-                              "WHERE ScheduleMessageID IS NOT NULL")
-            chats = await cursor.fetchall()
+        query = sql.SQL("SELECT * FROM chats WHERE schedule_message_id IS NOT NULL")
+        await self._cursor.execute(query)
+        chats = await self._cursor.fetchall()
         result = []
         for chat in chats:
             result.append(ChatModel(*chat))
