3439b1cefc40ebb0c6f72ca15f43c6d1019debc3
pichus <dimatyzuk@gmail.com>
2025-02-06 21:16:46 +0200

    fixed some errors, made code more readable

--- handlers/schedule.py
+++ handlers/schedule.py
@@ -9,6 +9,7 @@ from keyboards.chat_choice import chat_choice_keyboard
 
 from keyboards.schedule_choice import schedule_type_choice_keyboard
 from repositories import UserRepository, ChatRepository
+from repositories.schedule_repository import ScheduleRepository
 
 router = Router()
 
@@ -18,28 +19,32 @@ class ChooseScheduleType(StatesGroup):
     choosing_schedule_type = State()
     sending_schedule = State()
 
-
 @router.message(StateFilter(None), Command("add_schedule"))
 async def cmd_add_schedule(message: Message, state: FSMContext):
-    with UserRepository as user_repository:
-        user_chats = user_repository.get_user_chats(message.from_user.id)
+    user_repository = UserRepository()
+    async with user_repository:
+        user_chats = await user_repository.get_user_chats(message.from_user.id)
 
-    chat_names = []
-    for chat in user_chats:
-        chat_names.append(chat["chat_name"])
+    user_chat_names = []
+    for user_chat in user_chats:
+        user_chat_names.append(user_chat.chat_name)
 
     await message.answer("В якому чаті ви бажаєте оновити розклад?",
-                         reply_markup=chat_choice_keyboard(chat_names))
+                         reply_markup=chat_choice_keyboard(user_chat_names))
     await state.set_state(ChooseScheduleType.choosing_chat)
 
 
 @router.message(ChooseScheduleType.choosing_chat, F.text)
 async def choose_schedule_type(message: Message, state: FSMContext):
-    await message.answer(f"Ви обрали чат \"{message.text.lower()}\"")
-    await state.update_data(chosen_chat=message.text.lower())
+    await message.answer(f"Ви обрали чат \"{message.text}\"")
+    await state.update_data(chosen_chat_name=message.text)
+
+    schedule_repository = ScheduleRepository()
+    async with schedule_repository:
+        schedule_types = await schedule_repository.get_schedule_types()
 
     await message.answer("Який розклад ви бажаєте додати?",
-                         reply_markup=schedule_type_choice_keyboard(['Нижній', 'Верхній']))
+                         reply_markup=schedule_type_choice_keyboard(schedule_types))
     await state.set_state(ChooseScheduleType.choosing_schedule_type)
 
 @router.message(ChooseScheduleType.choosing_schedule_type, F.text.in_(['нижній', 'верхній']))
@@ -54,13 +59,16 @@ async def enter_schedule(message: Message, state: FSMContext):
 @router.message(ChooseScheduleType.sending_schedule, F.text)
 async def response(message: Message, state: FSMContext):
     user_data = await state.get_data()
-    with ChatRepository() as chat_repository:
-        if user_data["chosen_schedule_type"] == "нижній":
-            print(message.text)
-            chat_repository.update_low_schedule(message.text, user_data["chosen_chat"])
-        elif user_data["chosen_schedule_type"] == "верхній":
-            print(message.text)
-            chat_repository.update_high_schedule(message.text, user_data["chosen_chat"])
+
+    schedule_repository = ScheduleRepository()
+    chat_repository = ChatRepository()
+
+    async with chat_repository:
+        chat_telegram_id = await chat_repository.get_chat_telegram_id_by_name_and_user_telegram_id(
+            user_data["chosen_chat_name"], message.from_user.id)
+
+    async with schedule_repository:
+        await schedule_repository.upsert_schedule(chat_telegram_id, user_data["chosen_schedule_type"], message.text)
 
     await message.answer("Успішно додано розклад")
     await state.clear()
@@ -68,18 +76,18 @@ async def response(message: Message, state: FSMContext):
 
 @router.message(Command("send_schedule"))
 async def cmd_send_schedule(message: Message):
-    with ChatRepository() as chat_repository:
+    chat_repository = ChatRepository()
+    async with chat_repository:
         if not chat_repository.chat_exists(message.chat.id):
             await message.answer("Цього чату немає в базі даних бота. Додайте його командою /add_chat")
             return
 
-        schedules = chat_repository.get_schedules(message.chat.id)
-        current_schedule: str
+        current_week_type = utils.get_current_week_type()
 
-        if utils.is_high_week(datetime.datetime.now().isocalendar().week):
-            current_schedule = schedules["high"]
-        else:
-            current_schedule = schedules["low"]
+        schedule_repository = ScheduleRepository()
+        async with schedule_repository:
+            schedule = await schedule_repository.get_schedule(message.chat.id, current_week_type)
 
-        bot_message = await message.answer(current_schedule)
-        chat_repository.add_schedule_message_id(bot_message.message_id, message.chat.id)
+
+        bot_message = await message.answer(schedule)
+        await chat_repository.add_schedule_message_to_edit_id(bot_message.message_id, message.chat.id)
