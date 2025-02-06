e8c79b48c773f7e7ccc209f4f4828e7238082c89
pichus <dimatyzuk@gmail.com>
2025-02-06 21:20:46 +0200

    cleaned up the code in the add_chat function added a get_telegram_id_by_name_and_user_telegram_id function

--- repositories/chat_repository.py
+++ repositories/chat_repository.py
@@ -12,24 +12,25 @@ class ChatRepository(RepositoryBase):
         return user[0]
 
     async def add_chat(self, chat_model: ChatModel, user_telegram_id: int) -> bool:
+        result = True
         if await self.chat_exists(chat_model.chat_telegram_id):
-            return False
+            result = False
+        else:
+            query1 = sql.SQL("SELECT user_id FROM users WHERE user_telegram_id = %s")
+            await self._cursor.execute(query1, (user_telegram_id,))
+            user_id = (await self._cursor.fetchone())[0]
 
-        query1 = sql.SQL("SELECT user_id FROM users WHERE user_telegram_id = %s")
-        await self._cursor.execute(query1, (user_telegram_id,))
-        user_id = await self._cursor.fetchone()["user_id"]
+            query2 = sql.SQL(
+                "INSERT INTO chats (chat_telegram_id, message_thread_id, chat_name, user_id) VALUES (%s, %s, %s, %s)")
+            await self._cursor.execute(query2,(chat_model.chat_telegram_id, chat_model.message_thread_id,
+                                               chat_model.chat_name, int(user_id)))
 
-        query2 = sql.SQL(
-            "INSERT INTO chats (chat_telegram_id, message_thread_id, chat_name, user_id) VALUES (%s, %s, %s, %s)")
-        await self._cursor.execute(query2,(chat_model.chat_telegram_id, chat_model.message_thread_id,
-                                           chat_model.chat_name, int(user_id)))
+            await self._db_connection.commit()
 
-        await self._db_connection.commit()
+        return result
 
-        return True
-
-    async def add_schedule_message_id(self, schedule_message_id, chat_telegram_id):
-        query = sql.SQL("UPDATE chats SET schedule_message_id = %s WHERE chat_telegram_id = %s")
+    async def add_schedule_message_to_edit_id(self, schedule_message_id: int, chat_telegram_id: int):
+        query = sql.SQL("UPDATE chats SET schedule_message_to_edit_id = %s WHERE chat_telegram_id = %s")
         await self._cursor.execute(query,(schedule_message_id, chat_telegram_id))
         await self._db_connection.commit()
 
@@ -42,3 +43,16 @@ class ChatRepository(RepositoryBase):
             result.append(ChatModel(*chat))
 
         return result
+
+    async def get_chat_telegram_id_by_name_and_user_telegram_id(self, chat_name: str,
+                                                                user_telegram_id: int) -> int:
+        query = sql.SQL("""
+            SELECT chat_telegram_id 
+            FROM chats 
+            JOIN users on users.user_id = chats.user_id
+            WHERE chats.chat_name = %s AND users.user_telegram_id = %s""")
+        await self._cursor.execute(query, (chat_name, user_telegram_id))
+        result = await self._cursor.fetchone()
+        result_chat_telegram_id = result[0]
+
+        return result_chat_telegram_id
