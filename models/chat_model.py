f3cf8d343fe808abead5e09ae70890de7f44ca0d
pichus <dimatyzuk@gmail.com>
2025-02-06 21:21:22 +0200

    got rid of internal database ids as they do not need to be exposed for any python code

--- models/chat_model.py
+++ models/chat_model.py
@@ -3,9 +3,7 @@ from dataclasses import dataclass
 
 @dataclass
 class ChatModel:
-    chat_id: int
-    user_id: int
     chat_telegram_id: int
     chat_name: str
-    message_thread_id: int
-    schedule_message_to_edit_id: int
+    message_thread_id: int | None = None
+    schedule_message_to_edit_id: int | None = None
