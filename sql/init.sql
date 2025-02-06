adee34e994d51f0f82d4dd40ceb9d1ce5037755f
pichus <dimatyzuk@gmail.com>
2025-02-06 21:18:17 +0200

    changed X_telegram_id column types to BIGINT, because telegram ids can take up to 64 bits added a constraint on schedules table so that chat_id and schedule_type combinations are unique

--- sql/init.sql
+++ sql/init.sql
@@ -1,13 +1,13 @@
 CREATE TABLE IF NOT EXISTS users (
     user_id SERIAL PRIMARY KEY,
-    user_telegram_id INTEGER NOT NULL,
+    user_telegram_id BIGINT NOT NULL,
     user_name TEXT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS chats (
     chat_id SERIAL PRIMARY KEY,
     user_id INTEGER NOT NULL REFERENCES users (user_id),
-    chat_telegram_id INTEGER NOT NULL,
+    chat_telegram_id BIGINT NOT NULL,
     chat_name TEXT NOT NULL,
     message_thread_id INTEGER,
     schedule_message_to_edit_id INTEGER
@@ -26,5 +26,6 @@ CREATE TABLE IF NOT EXISTS schedules (
     schedule_id SERIAL PRIMARY KEY,
     chat_id INTEGER NOT NULL REFERENCES chats (chat_id),
     schedule_type VARCHAR(100) NOT NULL REFERENCES schedule_types (schedule_type),
-    schedule TEXT NOT NULL
+    schedule TEXT NOT NULL,
+    CONSTRAINT unique_chat_schedule UNIQUE (chat_id, schedule_type)
 )
