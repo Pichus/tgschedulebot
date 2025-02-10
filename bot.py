ef4e918c82a09de0d3a8140b4b59fd0c05cffe7c
pichus <dimatyzuk@gmail.com>
2025-02-10 22:04:27 +0200

    instead of hardcoding the job date, the program now retrieves it from the env file and converts it into UTC

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import asyncio
 import logging
 import os
+from datetime import datetime
 
 import psycopg
 from aiogram import Dispatcher
@@ -10,7 +11,9 @@ from apscheduler.schedulers.asyncio import AsyncIOScheduler
 import config
 from bot_instance import BotSingleton
 from handlers import commands, schedule
-from scheduler import edit_schedule_messages_in_all_chats_job
+from scheduler import edit_schedule_messages_in_all_chats_job, CronDate
+from utils import convert_cron_date_to_utc
+from pytz import timezone
 
 
 async def main():
@@ -27,24 +30,36 @@ async def main():
 
     jobstores = {"default": SQLAlchemyJobStore(url=config.database_url)}
 
-    scheduler = AsyncIOScheduler(timezone="Europe/Kyiv", jobstores=jobstores)
+    scheduler = AsyncIOScheduler(timezone=timezone("UTC"), jobstores=jobstores)
 
     dp.include_routers(schedule.router, commands.router)
 
     await bot.delete_webhook(drop_pending_updates=True)
+
+    cron_date = CronDate(
+        day_of_week=config.cron_day_of_week,
+        hour=config.cron_hour,
+        minute=config.cron_minute,
+    )
+    utc_cron_date = convert_cron_date_to_utc(
+        from_timezone=config.cron_timezone, cron_date=cron_date
+    )
 
     scheduler.start()
     if not scheduler.get_job("edit_schedule_messages_in_all_chats_job"):
         scheduler.add_job(
             edit_schedule_messages_in_all_chats_job,
             trigger="cron",
-            day_of_week="mon",
-            hour=0,
+            day_of_week=utc_cron_date.day_of_week,
+            hour=utc_cron_date.hour,
+            minute=utc_cron_date.minute,
             max_instances=1,
             misfire_grace_time=None,
             coalesce=True,
             id="edit_schedule_messages_in_all_chats_job",
         )
+
+    scheduler.print_jobs()
 
     await dp.start_polling(bot)
 
