4fab73e2712a742aafe030a3c04a3c97f8c4dbcc
pichus <dimatyzuk@gmail.com>
2025-02-14 17:54:00 +0200

    added a new custom exception

--- exceptions/__init__.py
+++ exceptions/__init__.py
@@ -1,3 +1,4 @@
 from .schedule_not_found import ScheduleNotFoundError
+from .same_schedule import SameScheduleError
 
-__all__ = ["ScheduleNotFoundError"]
+__all__ = ["ScheduleNotFoundError", "SameScheduleError"]
