dd1d8850baba37c00ac43106e42cb0124c682b0f
pichus <dimatyzuk@gmail.com>
2025-04-06 00:40:54 +0300

    added a type alias for a schedule dict, which makes the code a lot more readable moved some formatting logic from other functions to format_subject_string func added a get_group_indexes func replaces get_schedule_by_group_index_and_day with 2 different funcs: get_all_schedules which goes through all the groups, making the code more optimised, and process_schedule_dictionary which separates some processing logic away from spreadsheet parsing

--- parser/parser.py
+++ parser/parser.py
@@ -1,11 +1,13 @@
 import re
 from collections import defaultdict
-from typing import Any
+from typing import Any, TypeAlias
 
 import gspread
 import requests
 from google.oauth2.service_account import Credentials
 
+# dict[day, dict[time_interval, list["subject1", "subject2", ...]]]
+ScheduleDict: TypeAlias = dict[str, dict[str, list[str]]]
 
 spreadsheet_id = "1kfdlUUWgZ9PKdL4oqJi-Og_Bz--ll-hF2yYgpfrxF4U"
 sheet_name = "бакалаври"
@@ -51,12 +53,21 @@ def apply_merges(sheet_values: list[list], sheet_metadata: dict[str, Any]) -> No
             if cols < merge["endColumnIndex"]:
                 sheet_values[row].extend([""] * (merge["endColumnIndex"] - cols))
             for col in range(merge["startColumnIndex"], merge["endColumnIndex"]):
-                sheet_values[row][col] = sheet_values[merge["startRowIndex"]][merge["startColumnIndex"]]
+                sheet_values[row][col] = sheet_values[merge["startRowIndex"]][
+                    merge["startColumnIndex"]
+                ]
 
 
-def format_subject_string(string_to_format: str):
+# TODO make the code more readable
+def format_subject_string(string_to_format: str, time_interval: str) -> str:
+    time_interval = time_interval.replace("\n", "")
+    if string_to_format == "empty_subject":
+        return f"*{time_interval}*\n💤💤💤\n\n"
+
     string_to_format = re.sub(r"\s+", " ", string_to_format)
-    string_to_format = re.sub(r"\d\s?п/г", "", string_to_format) # maybe it should be removed
+    string_to_format = re.sub(
+        r"\d\s?п/г", "", string_to_format
+    )  # maybe it should be removed
     subject_name = re.findall(r"(^.+?)(?:\()", string_to_format)
     if subject_name:
         string_to_format = string_to_format.replace(subject_name[0].strip(), "")
@@ -65,10 +76,10 @@ def format_subject_string(string_to_format: str):
     classroom_numbers = re.findall(r"\d+", string_to_format)
     teacher_names = re.findall(r"(?:ас[.]|доц[.]|пр[.])\s*?(\w+)", string_to_format)
 
-    result_lines: list[str] = []
+    result_lines: list[str] = [f"*{time_interval}*\n"]
 
     if subject_name:
-        result_lines.append(subject_name[0].strip() + '\n')
+        result_lines.append(subject_name[0].strip() + "\n")
 
     longest_teacher_name_len = 0
 
@@ -78,81 +89,118 @@ def format_subject_string(string_to_format: str):
     if len(teacher_names) == len(classroom_numbers):
         for teacher_name, classroom_number in zip(teacher_names, classroom_numbers):
             space_count = longest_teacher_name_len - len(teacher_name)
-            result_lines.append(f"•{teacher_name} {" " * space_count}{classroom_number} каб.\n")
-    else:
-        result_lines.append(f"• {teacher_names[0]}\n")
+            result_lines.append(
+                f"•{teacher_name} {" " * space_count}{classroom_number} каб.\n"
+            )
+    elif len(teacher_names) > 0:
+        result_lines.append(f"•{teacher_names[0]}\n")
+
+    result_lines.append("\n")
 
     return "".join(result_lines)
 
 
-def get_schedule_by_group_index_and_day(arg_values, week_type: str, group_index) -> str:
+def char_to_num(char: str) -> int:
+    return ord(char.lower()) - 96
+
+
+def get_group_indexes(
+    arg_values: list[list[str]], group_indexes_row: int, group_indexes_start_col: int
+) -> list[str]:
+    group_indexes: list = []
+
+    for col in range(group_indexes_start_col, len(arg_values[group_indexes_row])):
+        group_index = arg_values[group_indexes_row][col]
+        if not group_index or group_index.isspace():
+            continue
+
+        group_indexes.append(group_index)
+
+    return group_indexes
+
+
+def get_all_schedules(
+    arg_values: list[list[str]],
+    group_indexes_row: int,
+    days_col: int,
+    time_interval_col: int,
+    group_indexes_start_col: int,
+) -> dict[str, ScheduleDict]:
     days = ["понеділок", "вівторок", "середа", "четвер", "п'ятниця"]
-    result_dict: dict[str, dict[str, list]] = {}
+    group_indexes = get_group_indexes(
+        arg_values, group_indexes_row, group_indexes_start_col
+    )
 
-    result_lines = []
+    result_dict: dict[str, ScheduleDict] = {}
 
-    for day in days:
-        result_dict[day] = defaultdict(list)
+    for group_index in group_indexes:
+        result_dict[group_index] = {}
 
-    group_index_col = arg_values[1].index(group_index)
+        for day in days:
+            result_dict[group_index][day] = defaultdict(list)
 
-    for value in arg_values:
-        value_day = "".join(value[0].split()).lower()
+    for row in arg_values:
+        value_day = "".join(row[days_col].split()).lower()
 
         if value_day not in days:
             continue
 
-        subject: str = value[group_index_col]
+        for col in range(group_indexes_start_col, len(row)):
+            group_index = arg_values[group_indexes_row][col]
+            if group_index not in group_indexes:
+                continue
 
-        if not subject or subject.isspace():
-            subject = "empty_subject"
+            subject = row[col]
 
-        time_interval = value[1]
-        time_interval_dict_key = time_interval
+            if not subject or subject.isspace():
+                subject = "empty_subject"
 
-        if (not result_dict[value_day].get(time_interval_dict_key)) or (
-                subject not in result_dict[value_day][time_interval_dict_key]):
-            result_dict[value_day][time_interval_dict_key].append(subject)
+            time_interval = row[time_interval_col]
+
+            if (time_interval not in result_dict[group_index][value_day]) or (
+                subject not in result_dict[group_index][value_day][time_interval]
+            ):
+                result_dict[group_index][value_day][time_interval].append(subject)
+
+    return result_dict
+
+
+def process_schedule_dictionary(
+    schedule_dict: ScheduleDict,
+) -> tuple[str, str]:
+    results: tuple[list[str], list[str]] = ([], [])
+
+    results[0].append("Розклад *ВЕРХНІЙ*\n\n")
+    results[1].append("Розклад *НИЖНІЙ*\n\n")
 
     day: str
     schedule_dict: dict[str, list]
-    for day, schedule_dict in result_dict.items():
-        result_lines.append(day.capitalize() + "\n\n")
+    for day, schedule_dict in schedule_dict.items():
+
+        beginning = f"*{day.capitalize()}*\n" + ("-" * 18 + "\n")
+        for result in results:
+            result.append(beginning)
 
         time: str
         subjects: list
         for time, subjects in schedule_dict.items():
-            subject_to_add = subjects[0]
-
-            if len(subjects) > 1:
-                index = 0 if week_type == "верхній" else 1
-                subject_to_add = subjects[index]
-            elif subject_to_add == "empty_subject":
+            if len(subjects) == 1 and subjects[0] == "empty_subject":
                 continue
 
-            if subject_to_add == "empty_subject":
-                subject_to_add = "чіл в пузатці"
+            if len(subjects) > 1:
+                for count, result in enumerate(results):
+                    result.append(format_subject_string(subjects[count], time))
             else:
-                subject_to_add = format_subject_string(subject_to_add)
+                for result in results:
+                    result.append(format_subject_string(subjects[0], time))
 
-            time = time.replace("\n", "")
-            result_lines.append(time + " " + subject_to_add + "\n\n")
-
-    return "".join(result_lines)
+    return "".join(results[0]), "".join(results[1])
 
 
 def main():
     apply_merges(values, obj["sheets"][0])
-    print(get_schedule_by_group_index_and_day(values, "верхній", "К-10"))
-
-#     string = """Українська та зарубіжна культура
-# (лек) 30 год
-# (кон) 2 год
-# пр. Сторожук С.В.
-#
-#
-# 	"""
-#     print(format_subject_string(string))
+    all_schedules = get_all_schedules(values, 1, 0, 1, 2)
+    print(process_schedule_dictionary(all_schedules["К-16"])[1])
 
 
 main()
