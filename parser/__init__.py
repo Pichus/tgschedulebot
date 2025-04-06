3503f3a867e86209e8875375c2b8aec72f7165fc
pichus <dimatyzuk@gmail.com>
2025-04-06 22:41:17 +0300

    added an init file to use parser as a package

---
+++ parser/__init__.py
@@ -0,0 +1,5 @@
+from .parser import fetch_schedules_dictionary
+from .parser import process_schedule_dictionary
+from .parser import ScheduleDict
+
+__all__ = ["fetch_schedules_dictionary", "process_schedule_dictionary", "ScheduleDict"]
