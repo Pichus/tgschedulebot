a03e13f524963afa6118cfd02352059ba8cba2cc
pichus <dimatyzuk@gmail.com>
2025-04-08 19:27:16 +0300

    added a new event that adds the chat to db automatically, making the add_chat command obsolete

--- handlers/commands.py
+++ handlers/commands.py
@@ -1,6 +1,6 @@
 from aiogram import Router
-from aiogram.filters import Command
-from aiogram.types import Message
+from aiogram.filters import Command, ChatMemberUpdatedFilter, IS_NOT_MEMBER, IS_MEMBER
+from aiogram.types import Message, ChatMemberUpdated
 
 from models import UserModel, ChatModel
 from repositories import UserRepository, ChatRepository
@@ -20,6 +20,7 @@ async def cmd_start(message: Message):
         await user_repository.add_user(user_to_add)
 
 
+# deprecated
 @router.message(Command("add_chat"))
 async def cmd_add_chat(message: Message):
     chat_repository = ChatRepository()
@@ -41,3 +42,22 @@ async def cmd_add_chat(message: Message):
         await message.answer("Успішно додано чат\n")
     else:
         await message.answer("Цей чат вже було додано раніше")
+
+
+@router.my_chat_member(ChatMemberUpdatedFilter(IS_NOT_MEMBER >> IS_MEMBER))
+async def on_user_join(event: ChatMemberUpdated):
+    user_telegram_id = event.from_user.id
+    user_name = event.from_user.full_name
+    chat_telegram_id = event.chat.id
+    chat_name = event.chat.full_name
+
+    chat_to_add = ChatModel(chat_telegram_id, chat_name)
+
+    user_repository = UserRepository()
+    async with user_repository:
+        user_to_add = UserModel(user_telegram_id, user_name)
+        await user_repository.add_user(user_to_add)
+
+    chat_repository = ChatRepository()
+    async with chat_repository:
+        await chat_repository.add_chat(chat_to_add, user_telegram_id)
